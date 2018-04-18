06aeb303844d8479a595dd61f50044fb797e1043
Lev Ostanin <lev.ostanin@gmail.com>
2018-04-18 19:41:40 +0300

    fix bad import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
